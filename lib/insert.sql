32839eb8278e4dfc21c72b2cb9deae560395aa31
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-20 19:15:24 +0000

    whew that was tough

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 45, 'M', 'blue', 'angry', 1);
+
+INSERT INTO bears VALUES (2, 'Rowdy', 45, 'M', 'blue', 'angry', 1);
+
+INSERT INTO bears VALUES (3, 'Tabitha', 6, 'F', 'blue', 'angry', 1);
+
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 45, 'M', 'blue', 'angry', 1);
+
+INSERT INTO bears VALUES (5, 'Melissa', 13, 'F', 'blue', 'angry', 1);
+
+INSERT INTO bears VALUES (6, 'Grinch', 45, 'M', 'blue', 'angry', 1);
+
+INSERT INTO bears VALUES (7, 'Wendy', 6, 'F', 'blue', 'angry', 1);
+
+INSERT INTO bears VALUES (8, NULL, 45, 'M', 'blue', 'angry', 1);
+
+
+
+
